9cf8f636f8245017b46710495bc5a8bb0cf44019
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-12-05 13:25:33 +0100

    Day 5

---
+++ src/advent_of_code_2021/day05.clj
@@ -0,0 +1,59 @@
+(ns advent_of_code_2021.day05
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn parse-int [s]
+  (Integer/parseInt s))
+
+(defn parse-line [l]
+  (let [[_ x1 y1 x2 y2] (re-matches #"(\d+),(\d+) -> (\d+),(\d+)" l)]
+    (vector (parse-int x1) (parse-int y1) (parse-int x2) (parse-int y2))))
+
+(defn generate-points-vertical [x y1 y2]
+  (let [diff (- y2 y1)
+        dy (if (pos-int? diff) 1 -1)]
+    (->> (iterate #(+ % dy) y1)
+         (map #(vector x %))
+         (take (inc (* dy diff))))))
+
+(defn generate-points-horizontal [x1 x2 y]
+  (let [diff (- x2 x1)
+        dx (if (pos-int? diff) 1 -1)]
+    (->> (iterate #(+ % dx) x1)
+         (map #(vector % y))
+         (take (inc (* dx diff))))))
+
+(defn generate-points-diagonal [x1 y1 x2 y2]
+  (let [diff-x (- x2 x1)
+        diff-y (- y2 y1)
+        dx (if (pos-int? diff-x) 1 -1)
+        dy (if (pos-int? diff-y) 1 -1)]
+    (->> (iterate #(inc %) 0)
+         (map #(vector (+ x1 (* % dx)) (+ y1 (* % dy))))
+         (take (inc (* dx diff-x))))))
+
+(defn generate-points [vents diagonal-vents?]
+  (apply concat (map (fn [[x1 y1 x2 y2]]
+                       (cond
+                         (= y1 y2) (generate-points-horizontal x1 x2 y1)
+                         (= x1 x2) (generate-points-vertical x1 y1 y2)
+                         diagonal-vents? (generate-points-diagonal x1 y1 x2 y2)
+                         :else (seq [])))
+                  vents)))
+
+(defn count-overlapping [vents diagonal-vents?]
+  (->> (generate-points vents diagonal-vents?)
+       frequencies
+       (filter #(>= (second %) 2))
+       count))
+
+(defn read-input []
+  (->> (io/resource "input-05.txt")
+       slurp
+       string/split-lines
+       (map parse-line)))
+
+(defn -main []
+  (let [vents (read-input)]
+    (println "Part 1:" (count-overlapping vents false))
+    (println "Part 2:" (count-overlapping vents true))))
