6b089f8283264e031bec05d2e483bb5532d1f748
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-12-06 20:59:33 +0100

    Day 6

---
+++ src/advent_of_code_2021/day06.clj
@@ -0,0 +1,28 @@
+(ns advent_of_code_2021.day06
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(def sum-nil #((fnil + 0) %1 %2))
+
+(defn parse-input []
+  (let [input (slurp (io/resource "input-06.txt"))]
+    (->> (string/split input #",")
+         (map #(Integer/parseInt %))
+         frequencies)))
+
+(defn next-day [state]
+  (reduce-kv (fn [m k v] (if (zero? k)
+                           (-> m
+                               (update 6 #(sum-nil % v))
+                               (update 8 #(sum-nil % v)))
+                           (update m (dec k) #(sum-nil % v))))
+    {} state))
+
+(defn count-fish [initial-state after-days]
+  (let [days (iterate next-day initial-state)]
+    (apply + (vals (nth days after-days)))))
+
+(defn -main []
+  (let [initial-state (parse-input)]
+    (println "Part 1:" (count-fish initial-state 80))
+    (println "Part 2:" (count-fish initial-state 256))))
