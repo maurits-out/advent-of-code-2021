3d46e886db6fae83d2aecdb773c39219928178ed
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2022-07-23 15:45:38 +0200

    Day 1

---
+++ src/main/scala/day01/SonarSweep.scala
@@ -0,0 +1,27 @@
+package day01
+
+import scala.io.Source
+import scala.util.Using
+
+def readMeasurements(): List[Int] =
+  Using.resource(getClass.getResourceAsStream("/day01.txt")) { stream =>
+    Source.fromInputStream(stream).getLines().map(_.toInt).toList
+  }
+
+@main
+def part1(): Unit = {
+  val answer = readMeasurements()
+    .sliding(2)
+    .count { case Seq(a, b) => a < b }
+  println(s"Part 1: $answer")
+}
+
+@main
+def part2(): Unit = {
+  val answer = readMeasurements()
+    .sliding(3)
+    .map(_.sum)
+    .sliding(2)
+    .count { case Seq(a, b) => a < b }
+  println(s"Part 2: $answer")
+}
