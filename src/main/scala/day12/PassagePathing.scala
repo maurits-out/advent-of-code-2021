b6d497e5b8eb6faff8782130dd32e784d274701e
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2022-10-16 16:59:47 +0200

    Day 12

---
+++ src/main/scala/day12/PassagePathing.scala
@@ -0,0 +1,46 @@
+package day12
+
+import sun.security.provider.certpath.AdjacencyList
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+import scala.io.Source
+import scala.util.Using
+import scala.util.chaining.scalaUtilChainingOps
+
+class PassagePathing(input: String):
+
+  private val adjacencyList: Map[String, List[String]] =
+    input.linesIterator
+      .flatMap {
+        case s"$v-$w" => Seq(v -> w, w -> v)
+      }
+      .toList
+      .groupMap((v, _) => v)((_, w) => w)
+
+  private def isLarge(cave: String): Boolean = cave.forall(_.isUpper)
+
+  private def countPaths(path: Seq[String] = Seq("start"), secondVisitCheck: Seq[String] => Boolean): Int =
+    path.head match
+      case "end" => 1
+      case cave => adjacencyList(cave)
+        .collect {
+          case c if isLarge(c) || !path.contains(c) || secondVisitCheck(path) => countPaths(c +: path, secondVisitCheck)
+        }
+        .sum
+
+  def part1(): Int = countPaths(secondVisitCheck = _ => false)
+
+  def part2(): Int = countPaths(secondVisitCheck = _.filterNot(isLarge).pipe(path => path.size == path.distinct.size))
+end PassagePathing
+
+@main
+def main(): Unit =
+  def readInput(): String =
+    Using.resource(getClass.getResourceAsStream("/day12.txt")) { stream =>
+      Source.fromInputStream(stream).mkString
+    }
+
+  val passagePathing = new PassagePathing(readInput())
+  println(s"Part 1: ${passagePathing.part1()}")
+  println(s"Part 2: ${passagePathing.part2()}")
