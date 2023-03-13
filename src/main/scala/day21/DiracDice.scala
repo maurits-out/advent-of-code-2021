b91f4300d311a4344be1293da825607e2b44b189
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2023-03-13 21:32:06 +0100

    Day 21

--- src/main/scala/day21/DiracDice.scala
+++ src/main/scala/day21/DiracDice.scala
@@ -20,17 +20,17 @@ class DiracDice(start1: Int, start2: Int):
 
 
   def part2(): Long =
-    val rollFreq = List(3 -> 1, 4 -> 3, 5 -> 6, 6 -> 7, 7 -> 6, 8 -> 3, 9 -> 1)
+    val rollCount = List(3 -> 1, 4 -> 3, 5 -> 6, 6 -> 7, 7 -> 6, 8 -> 3, 9 -> 1)
 
     def countWins(space1: Int, score1: Int, space2: Int, score2: Int): (Long, Long) =
       score2 match
         case s if s >= 21 => (0, 1)
         case _ =>
           val w =
-            for (roll, freq) <- rollFreq
+            for (roll, count) <- rollCount
                 nextSpace = (space1 + roll) % 10
                 (c2, c1) = countWins(space2, score2, nextSpace, score1 + nextSpace + 1)
-            yield (freq * c1, freq * c2)
+            yield (count * c1, count * c2)
           (w.map(_._1).sum, w.map(_._2).sum)
 
     val (w1, w2) = countWins(start1 - 1, 0, start2 - 1, 0)
