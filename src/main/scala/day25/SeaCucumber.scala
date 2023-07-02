64498b2243b1bee872e05224f22141146d04025c
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2023-07-02 15:40:44 +0200

    Small improvements.

--- src/main/scala/day25/SeaCucumber.scala
+++ src/main/scala/day25/SeaCucumber.scala
@@ -21,7 +21,7 @@ class SeaCucumber(input: String):
         stepUntilSeaCucumbersStopMoving(nextState, count + 1)
 
     stepUntilSeaCucumbersStopMoving(parseInput(), 1)
-
+  end part1
 
   private def stepEast(state: State): State =
     def moveEastIfAllowed(location: Location): Location =
@@ -32,6 +32,7 @@ class SeaCucumber(input: String):
       if state.eastFacing.contains(next) || state.southFacing.contains(next) then location else next
 
     state.copy(eastFacing = state.eastFacing.map(moveEastIfAllowed))
+  end stepEast
 
   private def stepSouth(state: State): State =
     def moveSouthIfAllowed(location: Location): Location =
@@ -42,6 +43,7 @@ class SeaCucumber(input: String):
       if state.eastFacing.contains(next) || state.southFacing.contains(next) then location else next
 
     state.copy(southFacing = state.southFacing.map(moveSouthIfAllowed))
+  end stepSouth
 
   private def parseInput(): State =
     val rows = input.linesIterator.toSeq
@@ -51,12 +53,16 @@ class SeaCucumber(input: String):
       eastFacing = extractLocations(rows, '>'),
       southFacing = extractLocations(rows, 'v')
     )
+  end parseInput
 
   private def extractLocations(rows: Seq[String], cucumberType: Char): Set[Location] =
     (for (line, r) <- rows.zipWithIndex
          (ch, c) <- line.zipWithIndex
          if ch == cucumberType
     yield Location(r, c)).toSet
+  end extractLocations
+
+end SeaCucumber
 
 @main
 def main(): Unit =
@@ -65,3 +71,4 @@ def main(): Unit =
   }
   val seaCucumber = new SeaCucumber(input)
   println(s"Part 1: ${seaCucumber.part1()}")
+end main
